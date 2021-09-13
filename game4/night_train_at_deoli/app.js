1a60cee967dbf4b7d143299286886a3cefa00586
DaryaGorbachova <vrindavaneshvari@mail.ru>
2021-09-13 19:00:53 +0300

    3 tasks done in night train

--- game4/night_train_at_deoli/app.js
+++ game4/night_train_at_deoli/app.js
@@ -1,22 +1,31 @@
 const tasks = document.querySelectorAll('.task');
 const task1 = document.querySelectorAll(".task1");
 const checkbtn = document.querySelectorAll('.submit');
-const answers1 = document.querySelectorAll('.task1 input');
+const answers1 = document.querySelectorAll('.form1 input');
 const labels = document.querySelectorAll('.io');
-const outputCorAns = document.getElementById('outputCorAns');
-const tryagainbtn = document.querySelector('.try');
-const warning = document.getElementById('warning');
+const outputCorAns = document.querySelectorAll('.outputCorAns');
+const tryagainbtn = document.querySelectorAll('.try');
+const warning = document.querySelectorAll('.warning');
+
+const  dragaims = document.querySelectorAll('.dragaim span');
+const dragtext = document.querySelectorAll(".drag p");
+const dragfield = document.querySelector(".drag");
+
+const answers3 = document.querySelectorAll('.fillgaps input');
 
     let present = 'show';
     let answers = [];
     let correct1 = 0;
-    const correctAnswers = [3, 6, 10, 11, 16, 19]
-tasks.forEach(task => task.addEventListener('click', (e) => {
+    const correctAnswers = [3, 6, 10, 11, 16, 19];
+    const dragAnswers = ['drag2', 'drag1', 'drag5', 'drag3', 'drag4'];
+    const fillAnswers = ['helplessly', 'restless', 'further', 'impatient', 'discovering', 'unchanged']
 
-    if(e.target.id == 'questions') showHide(0, present)
-    else if (e.target.id == 'matching') showHide(1, present)
-    else if (e.target.id == 'discussion') showHide(2, present)
-    else if (e.target.id == 'transcription') showHide(3, present)
+tasks.forEach(task => task.addEventListener('click', (e) => {
+    if(e.target.id == 'prelistening') showHide(0, present)
+    else if(e.target.id == 'questions') showHide(1, present)
+    else if (e.target.id == 'fillgaps') showHide(2, present)
+    else if (e.target.id == 'discussion') showHide(3, present)
+    else if (e.target.id == 'transcription') showHide(4, present)
 }))
 
 function showHide (el, action) {
@@ -28,16 +37,16 @@ function showHide (el, action) {
 
 function checkQuestions () {
     correct1 = 0;
-    if (answers.length > 0) answers = [];
+    answers = [];
     for (let i = 0; i < answers1.length; i++) {
         if (answers1[i].checked === true) answers.push(i)
     }
 
     if (answers.length !== correctAnswers.length) {
-        warning.innerHTML = 'Answer all the questions first!'
+        warning[1].innerHTML = 'Answer all the questions first!'
         return false;
     } 
-    warning.style.display = 'none';
+    warning[1].style.display = 'none';
     for (let i = 0; i < answers.length; i++) {
         if (answers[i] === correctAnswers[i]) {
             correct1++;
@@ -49,32 +58,112 @@ function checkQuestions () {
             labels[answers[i]].style.backgroundColor = 'var(--lightred)'
             labels[answers[i]].style.color = 'var(--red)'    
         }
-        outputCorAns.style.opacity = '100%';
-        outputCorAns.innerHTML = `${correct1}/${correctAnswers.length}`;
-        checkbtn[0].style.opacity = '0';
-        tryagainbtn.style.opacity = '100%';
+        outputCorAns[1].style.opacity = '100%';
+        outputCorAns[1].innerHTML = `${correct1}/${correctAnswers.length}`;
+        checkbtn[1].style.opacity = '0';
+        tryagainbtn[0].style.opacity = '100%';
         if (correct1 === correctAnswers.length) {
-                warning.innerHTML = 'Congratulations!'
-                warning.style.animation = 'color-size-change 4s infinite'
+            warning[1].style.display = 'block';
+                warning[1].innerHTML = 'Congratulations!';
+                warning[1].style.animation = 'color-size-change 4s infinite';
         }
     }
 }
 
-function tryagain () {
+function tryagain (ex) {
     answers = [];
     correct1 = 0;
+    if (ex === 1) {
     for (let i = 0; i < labels.length; i++) {
     labels[i].style.backgroundColor = 'var(--white)';
     labels[i].style.color = 'var(--black)';
     }
-    checkbtn[0].style.opacity = '100%';
-    tryagainbtn.style.opacity = '0%';
+    checkbtn[1].style.opacity = '100%';
+    tryagainbtn[0].style.opacity = '0%';
+}
 }
 
-checkbtn[0].addEventListener('click', ()=> {
+checkbtn[1].addEventListener('click', ()=> {
     checkQuestions();
 })
 
-tryagainbtn.addEventListener('click', () => {
-    tryagain();
+tryagainbtn[0].addEventListener('click', () => {
+    tryagain(1);
+})
+
+
+
+function checkPrelistening () {
+    warning[0].style.display = 'none';
+    correct1 = 0;
+    for (let i = 0; i < dragaims.length; i++){
+        if (dragaims[i].children[0].id === dragAnswers[i]) {
+            correct1++;
+        }
+}
+outputCorAns[0].style.opacity = '100%';
+outputCorAns[0].innerHTML = `${correct1}/${dragAnswers.length}`;
+if (correct1 === dragAnswers.length) {
+    warning[0].style.display = 'block';
+    warning[0].innerHTML = 'There you go!'
+    warning[0].style.animation = 'color-size-change 4s infinite'
+}
+}
+
+
+checkbtn[0].addEventListener('click', () => {
+    for (let i = 0; i < dragaims.length; i++){
+    if (dragaims[i].children.length < 1) {
+        warning[0].innerHTML = 'Finish the task first!';
+        return false;
+    }
+
+}
+    checkPrelistening();
+})
+
+
+dragtext.forEach(dragtext => dragtext.addEventListener('click', ev => {
+    dragfield.appendChild(ev.target);
+}))
+
+function dealWithInput () {
+    answers = [];
+    let answer = '';
+    for (let i = 0; i < fillAnswers.length; i++) {
+    answer = answers3[i].value;
+    if (answer.slice(-1) === ' ') {
+        answer = answers3[i].value.slice(0, answer.length - 1);
+    }
+    if (answer[0] === ' ') answer = answer.slice(1, answer.length);
+    answer = answer.toLowerCase();
+    answers.push(answer);
+    }
+    return answers;
+}
+
+function checkFill () {
+    correct1 = 0;
+    for (let i = 0; i < fillAnswers.length; i++) {
+        if (answers[i] === fillAnswers[i]) {
+            correct1++;
+            answers3[i].style.backgroundColor = 'var(--lightgreen)';
+            answers3[i].style.border = 'solid 3px var(--green)';
+        } else {
+            answers3[i].style.backgroundColor = 'var(--lightred)';
+            answers3[i].style.border = 'solid 3px var(--red)';
+    }
+    outputCorAns[2].style.opacity = '100%';
+    outputCorAns[2].innerHTML = `${correct1}/${fillAnswers.length}`;
+}
+if (correct1 === fillAnswers.length) {
+    warning[2].style.display = 'block';
+    warning[2].innerHTML = 'Good job!';
+    warning[2].style.animation = 'color-size-change 4s infinite';
+}
+}
+
+checkbtn[2].addEventListener('click', () => {
+    dealWithInput();
+    checkFill();
 })
